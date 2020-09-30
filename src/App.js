69405e2bee4741fe7772691046efe6533dd9bcfc
Stephen <stephenparkhum@outlook.com>
2020-09-30 07:25:03 -0700

    Updating meta tags and keywords for social media and Google indexing

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import './styles/styles.css';
 // Component
 import Main from './components/Main/Main'
 import Header from './components/Header/Header'
+import HelmetData from './components/HelmetData/HelmetData'
 
-// Helmet Elements 
-import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <div className="App">
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Maestus | Official Site</title>
-        <meta name="description" content="Maestus is a blackened doom band from the Pacific Northwest" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="canonical" href="https://maest.us" />
-      </Helmet>
+      <HelmetData />
       <Header />
       <Main />
     </div>
