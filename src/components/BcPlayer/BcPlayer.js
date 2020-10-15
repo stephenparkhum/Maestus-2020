867ffa2e8e5a912aa488f6d0cee63b2e07b5cc85
Stephen <stephenparkhum@outlook.com>
2020-10-15 16:52:03 -0700

    Adding random song functionality for BC player

--- src/components/BcPlayer/BcPlayer.js
+++ src/components/BcPlayer/BcPlayer.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
 const BcPlayer = () => {
+    const songData = [
+        '3241988708', 
+        '440411643', 
+        '705618272',
+        '3338469584'
+    ];
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+      }
+
+    const getRandomTrack = (songData) => {
+        let randomInt = getRandomInt(songData.length - 1);
+        let trackSrc = `https://bandcamp.com/EmbeddedPlayer/album=1609786598/size=small/bgcol=333333/linkcol=0f91ff/track=${songData[randomInt]}/transparent=true/`
+        return trackSrc;
+    }
+
     return ( 
         <div className="bc--player">
-            <iframe title="Maestus Bandcamp Player" style={{border: '0', width: '100%', height: '42px'}} src="https://bandcamp.com/EmbeddedPlayer/album=1609786598/size=small/bgcol=333333/linkcol=0f91ff/track=440411643/transparent=true/" seamless><a href="https://maestus.bandcamp.com/album/deliquesce">Deliquesce by Maestus</a></iframe>
+            <iframe title="Maestus Bandcamp Player" style={{border: '0', width: '100%', height: '42px'}} src={getRandomTrack(songData)} seamless><a href="https://maestus.bandcamp.com/album/deliquesce">Deliquesce by Maestus</a></iframe>
         </div>
     )
 }
