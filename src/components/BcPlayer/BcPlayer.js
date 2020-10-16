26094b04dd97ba10888100403a069139ee6f491a
Stephen <stephenparkhum@outlook.com>
2020-10-15 21:38:56 -0700

    Updating BC player fuc

--- src/components/BcPlayer/BcPlayer.js
+++ src/components/BcPlayer/BcPlayer.js
@@ -12,29 +12,26 @@ const BcPlayer = () => {
             '705618272',
             '3338469584'
           ]
-        }, 
+        }
     ]
-
-    const songData = [
-      '3241988708', 
-      '440411643', 
-      '705618272',
-      '3338469584'
-    ];
 
     function getRandomInt(max) {
         return Math.floor(Math.random() * Math.floor(max));
       }
 
-    const getRandomTrack = (songData) => {
-        let randomInt = getRandomInt(songData.length - 1);
-        let trackSrc = `https://bandcamp.com/EmbeddedPlayer/album=1609786598/size=small/bgcol=333333/linkcol=0f91ff/track=${songData[randomInt]}/transparent=true/`
+    const getRandomTrack = (albumData) => {
+        let albumId = albumData[0].id;
+        let tracks = albumData[0]['tracks'];
+        let randomInt = getRandomInt(tracks.length - 1);
+        let trackSrc = `https://bandcamp.com/EmbeddedPlayer/album=${albumId}/size=small/bgcol=333333/linkcol=0f91ff/track=${tracks[randomInt]}/transparent=true/`
         return trackSrc;
     }
 
+    const albumMachineName = `https://maestus.bandcamp.com/album/${albumData[0].machine_name}`
+
     return ( 
         <div className="bc--player">
-            <iframe title="Maestus Bandcamp Player" style={{border: '0', width: '100%', height: '42px'}} src={getRandomTrack(songData)} seamless><a href="https://maestus.bandcamp.com/album/deliquesce">Deliquesce by Maestus</a></iframe>
+            <iframe title="Maestus Bandcamp Player" style={{border: '0', width: '100%', height: '42px'}} src={getRandomTrack(albumData)} seamless><a href={albumMachineName}>{albumData[0].name} by Maestus</a></iframe>
         </div>
     )
 }
